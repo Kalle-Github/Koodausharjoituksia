483db121a1146463fb4602e1bb38260767a2d477
Kalle-Github <kalle23011@student.hamk.fi>
2023-12-07 06:44:32 +0200

    Lisättiin taulukko

---
+++ src/ListatCodeRunner/TaulukkoHarjoitus.java
@@ -0,0 +1,20 @@
+package ListatCodeRunner;
+import java.util.Arrays;
+
+public class TaulukkoHarjoitus {
+
+	public static void main(String[] args) {
+		
+		
+		int[] lampotilat = new int[7];
+		
+		lampotilat[0] = 18;
+		lampotilat[1] = 19;
+		lampotilat[2] = 24;
+		lampotilat[3] = 22;
+		lampotilat[4] = 16;
+		lampotilat[5] = 15;
+		lampotilat[6] = 14;
+		
+	}
+}
