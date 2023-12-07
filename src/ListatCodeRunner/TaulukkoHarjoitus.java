39abd120d1b5107d8401ec237e6e7e90023175d6
Kalle-Github <kalle23011@student.hamk.fi>
2023-12-07 06:46:16 +0200

    Järjestely
    Tehdään tarvittavat järjestelyt. Kuten lämpötilat kylmyysjärjestykseen ja katsotaan viikon lämpimin ja kylmin lämpötila

--- src/ListatCodeRunner/TaulukkoHarjoitus.java
+++ src/ListatCodeRunner/TaulukkoHarjoitus.java
@@ -16,5 +16,32 @@ public class TaulukkoHarjoitus {
 		lampotilat[5] = 15;
 		lampotilat[6] = 14;
 		
+		// Keskiviikon lämpötilan tulostus, eli 3. solu, indeksi 2
+		System.out.println(lampotilat[2]);
+		
+		// Koko taulokon tulostus
+		
+		int summa = 0;
+		
+		System.out.println("Viikon lämpötilat");
+		
+		for (int i = 0; i < lampotilat.length ; i++) {
+			System.out.println(lampotilat[i]);
+			summa = summa + lampotilat[i];
+			}
+		System.out.println("Summa on");
+	System.out.println(summa);
+	System.out.println("Järjestetään taulukko");
+	//järjestää taulukon
+	Arrays.sort(lampotilat);
+	
+	for (int i = 0; i < 7; i++){
+		System.out.println(lampotilat[i]);
 	}
+		
+	System.out.println("kylmin lämpötila on " + lampotilat[0]);
+	System.out.println("Lämpimin lämpötila on " + lampotilat[lampotilat.length-1]);
+		
+	}
+
 }
